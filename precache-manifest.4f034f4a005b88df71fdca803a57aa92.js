871753b7ce6b0bd05c67be11a82929319d9f7220
unknown <33037196+Aaerialys@users.noreply.github.com>
2020-10-15 21:46:16 -0400

    Updates

---
+++ precache-manifest.4f034f4a005b88df71fdca803a57aa92.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4da4ca06d5c84d9bd6ca83d8f951bcbd",
+    "url": "/speedforces/index.html"
+  },
+  {
+    "revision": "b014ba56b689b5bdf9de",
+    "url": "/speedforces/static/css/main.2d511f1f.chunk.css"
+  },
+  {
+    "revision": "ff116264c3c88a536b78",
+    "url": "/speedforces/static/js/2.6f368fa2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/speedforces/static/js/2.6f368fa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b014ba56b689b5bdf9de",
+    "url": "/speedforces/static/js/main.b00b4b79.chunk.js"
+  },
+  {
+    "revision": "12904e6d987f8a35d0c1",
+    "url": "/speedforces/static/js/runtime-main.e77a7b72.js"
+  },
+  {
+    "revision": "60a6c051c1563a067dd7166123a58698",
+    "url": "/speedforces/static/media/FORTE.60a6c051.TTF"
+  },
+  {
+    "revision": "1bf71be111189e76987a4bb9b3115cb7",
+    "url": "/speedforces/static/media/OpenSans-Light.1bf71be1.ttf"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "/speedforces/static/media/OpenSans-Regular.629a55a7.ttf"
+  },
+  {
+    "revision": "3ef9183025d87f0407c8cbaa30fa7363",
+    "url": "/speedforces/static/media/logo.3ef91830.ico"
+  }
+]);
