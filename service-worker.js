5aba0f46d415f28b500d60abcd9f6ecda6df414b
unknown <33037196+Aaerialys@users.noreply.github.com>
2020-10-16 20:00:41 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/speedforces/precache-manifest.c3c2711878160bf1bde4a87292eb9244.js"
+  "/speedforces/precache-manifest.7d4e75211a38bfa657efe13bf505a93f.js"
 );
 
 self.addEventListener('message', (event) => {
